1d4169e17e500ae3b47d722d962065ee22582fc5
Thiago Fernando <thiago@MacBook-de-Thiago.local>
2019-03-05 13:48:20 -0400

    aula 1 ate aula 9

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com cachorro', false));
+    this.todos.push(new Todo(2, 'ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'cortar cabelo', true));
+  }
+  alterarTexto(){
+    this.title = 'Alterado';
+  }
 }
---
+++ todo/src/models/todo.model.ts
@@ -0,0 +1,12 @@
+export class Todo{
+  /**
+   *
+   */
+  constructor(
+    public id: Number = 1,
+    public title: String,
+    public done: Boolean
+  ) {
+
+  }
+}
