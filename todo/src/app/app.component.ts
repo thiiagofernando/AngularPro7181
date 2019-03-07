9259e8249b9df39bbdae79cadc910461dd34c51a
Thiago Fernando <thiago@MacBook-de-Thiago.local>
2019-03-06 21:34:53 -0400

    aula de 14 a 16

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,33 +1,41 @@
-import { Component } from '@angular/core';
-import { Todo } from 'src/models/todo.model';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { Todo } from "src/models/todo.model";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   public todos: Todo[] = [];
-  public title: String = 'Minhas Tarefas';
+  public title: String = "Minhas Tarefas";
   public form: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      title: ['', Validators.compose([
-        Validators.minLength(3),
-        Validators.maxLength(11),
-        Validators.required
-      ])]
+      title: [
+        "",
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(60),
+          Validators.required
+        ])
+      ]
     });
-    this.todos.push(new Todo(1, 'Passear com cachorro', false));
-    this.todos.push(new Todo(2, 'ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'cortar cabelo', true));
   }
-  alterarTexto(){
-    this.title = 'Alterado';
+  alterarTexto() {
+    this.title = "Alterado";
   }
-
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+  clear(){
+    this.form.reset();
+  }
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
     if (index !== -1) {
